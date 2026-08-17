9966e36b4bd097d99d21f2edcb5a81e6427b8633
Nihit Jain <nihitjain690@gmail.com>
2026-08-17 17:34:06 +0530

    Prepare FastAPI backend for deployment

--- api.py
+++ api.py
@@ -22,9 +22,10 @@ app.include_router(agent_router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://nihit-jain.github.io",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
